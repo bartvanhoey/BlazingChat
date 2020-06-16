4f6b9825b1be87f6cbeafe5fadfb9d5e50dfab45
bartvanhoey <bartvanhoey@hotmail.com>
2020-06-16 18:12:35 +0200

    end-EP06

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# BlazingChat" 
+# CuriousDrive BlazingChat 
+
+## ScaffoldCommand 
+
+`dotnet ef dbcontext scaffold Name=BlazingChat microsoft.entityframeworkcore.sqlite -o Models`
---
+++ Server/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using BlazingChat.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BlazingChat.Server.Models;
+
+namespace BlazingChat.Server.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class UsersController : ControllerBase
+  {
+    private readonly ILogger<UsersController> _logger;
+    private readonly BlazingChatContext _context;
+
+    public UsersController(ILogger<UsersController> logger, BlazingChatContext context)
+    {
+      _context = context;
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public List<Contact> Get()
+    {
+
+      List<User> users = _context.Users.ToList();
+      List<Contact> contacts = new List<Contact>();
+
+    foreach (var user in users)
+    {
+        contacts.Add(new Contact { FirstName = user.FirstName, LastName = user.LastName});
+    }
+
+return contacts;
+
+    }
+  }
+}
